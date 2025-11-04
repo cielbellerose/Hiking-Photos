78cdcebd6b4a6a393b38deedec0f5aebb63271c8
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-04 08:41:21 -0700

    Various changes to get communication accross the backend server -> frontend server wroking

--- backend.js
+++ backend.js
@@ -1,6 +1,6 @@
 import express from "express";
 import staticTestCoodinates from "./db/testData.json" with {type:"json"}; //this should be removed after db migration
-
+import cors from "cors"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,7 +8,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//This should be removed after db shifts from local
+//to prod
+//This is there to resolve issues with another server 
+//requesting data from this one
+app.use(cors());
 
+
+const date = new Date();
+console.log("server started");
 
 //routes to user pictures
 app.use("/user_data/", express.static("./user_data/"));
@@ -23,7 +31,7 @@ app.listen(PORT, () => {
 //this route just returns static files test JSON data to not have to deal with
 //seting up data flows quite yet
 app.use("/api/test",(req,res) => {
-  console.log("Sending JSON test data",staticTestCoodinates);
-  res.json(staticTestCoodinates);
+  console.log("Sending JSON test data",date.getMinutes(),date.getSeconds());
+  res.json({staticTestCoodinates,});
 });
 
