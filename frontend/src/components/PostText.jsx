37f8eb786780dd054c32681a8be9bb94ac9ed6ca
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-11 08:17:02 -0700

    Adeed text for page

---
+++ frontend/src/components/PostText.jsx
@@ -0,0 +1,9 @@
+export default function PostText({post}){
+
+    return(
+    <div className="post">
+        <div className="post-title">{post.title}</div>
+        <div>{post.text}</div>
+    </div>
+    );
+}
--- frontend/src/pages/PresentationPage.jsx
+++ frontend/src/pages/PresentationPage.jsx
@@ -3,17 +3,20 @@ import { useLocation } from 'react-router-dom';
 import { useState } from "react";
 import ServerConnector from "../modules/ServerConnector.js"
 import Map from "../components/Map.jsx"
+import PostText from "../components/PostText.jsx";
 
 export default function PresentationPage() {
   const [openPic, setOpenPic] = useState(() => -1);
   const [percent, setCurrentPercent] = useState(() => -1);
   const location = useLocation();
   const url = ServerConnector.getURLforMap(location.state.user,location.state.Percent1,location.state.Percent2); //format the query appropriately
-  console.log(url)
+  const post = {text:location.state.text,title:location.state.title}
+  console.log("Api endpoint" ,url);
   return (
     <>
       <TrailNavbar />
       <h1>Presentation Page</h1>
+      <PostText post={post} ></PostText>
       <Map url={url} percent={percent} setCurrentPercent={setCurrentPercent} openPic={openPic} setOpenPic={setOpenPic}/>
 
       <div className="contentContainer"></div>
