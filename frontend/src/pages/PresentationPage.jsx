7dcc8cba4cfbd7031a4d51bc34c4c8ac8f04a902
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-11 17:54:46 -0700

    made map work?

--- frontend/src/pages/PresentationPage.jsx
+++ frontend/src/pages/PresentationPage.jsx
@@ -1,19 +1,30 @@
 import TrailNavbar from "../components/Navbar";
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ServerConnector from "../modules/ServerConnector.js";
 import Map from "../components/Map.jsx";
 import PostText from "../components/PostText.jsx";
+import userGetter from "../modules/user.js"
 
 export default function PresentationPage() {
   const [openPic, setOpenPic] = useState(() => -1);
   const [percent, setCurrentPercent] = useState(() => -1);
   const location = useLocation();
-  const url = ServerConnector.getURLforMap(
-    location.state.user,
+  const [url, setUrl] = useState("");
+
+
+  useEffect( async () => {
+    const programUser = await userModule.getCurrentUser();
+    const urlgot = ServerConnector.getURLforMap(
+    programUser,
     location.state.Percent1,
     location.state.Percent2
-  ); //format the query appropriately
+  );
+  setUrl(urlgot)
+  },[])
+
+
+   //format the query appropriately
   const post = { text: location.state.text, title: location.state.title };
   console.log("Api endpoint", url);
   return (
@@ -28,15 +39,7 @@ export default function PresentationPage() {
         openPic={openPic}
         setOpenPic={setOpenPic}
       />
-
       <div className="contentContainer"></div>
-      <Map
-        url={ServerConnector.serverName + "/api/test"}
-        openPic={openPic}
-        setOpenPic={(v) => {
-          setOpenPic(v);
-        }}
-      />
     </>
   );
 }
